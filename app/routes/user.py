b12f09d21746e5422e409537113c540cbe365872
Antoine <Dewynter.cyber@outlook.fr>
2021-06-23 16:18:31 +0200

    save charles

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,8 +1,10 @@
 import datetime
+from sqlalchemy.sql import func
+from operator import and_
 from fastapi import APIRouter
 from app.config.database import conn
 from app.models.index import users,messages
-from app.schemas.index import User
+from app.schemas.index import User,Message
 from app.fonction.fonctionAPI import ml_emotion
 user = APIRouter()
 
@@ -51,21 +53,34 @@ async def create_message(user_id:int,dm_text:str):
     ml_emotion(dm_text)
     return 'Message créé'
 
-@user.post("/edit_message/{user_id}")
+@user.put("/edit_message/{user_id}")
 async def edit_message(user_id:int,dm_text:str):
     """use for edit message of user"""
     conn.execute(messages.update().values(
     user_id = user_id  ,
-    dm_text = dm_text ).where((messages.c.dm_datetime == datetime.date.today()) and (messages.c.user_id == user_id) ))
+    dm_text = dm_text ).where(and_(messages.c.dm_datetime == datetime.date.today() , messages.c.user_id == user_id) ))
     ml_emotion(dm_text)
     return 'Message Edité'
 
-@user.post("/list_message/{user_id}")
+@user.get("/list_message/{user_id}")
 async def list_message(user_id:int):
     """use for list message of user"""
     return conn.execute(messages.select().where(messages.c.user_id==user_id)).fetchall()
 
-@user.post("/read_message_emotion/{user_id}")
+@user.get("/read_message_emotion/{user_id}")
 async def read_message_emotion(user_id:int,dm_datetime):
     """use for read the emotion for a day and user"""
-    return conn.execute(messages.select().where((messages.c.dm_datetime == dm_datetime) and (messages.c.user_id==user_id) )).fetchall()
+    return conn.execute(messages.select().where(and_(messages.c.dm_datetime == dm_datetime,messages.c.user_id==user_id) )).fetchall()
+
+@user.get("/read_moyenne_emotion_periode/{user_id}")
+async def read_message_emotion_periode(user_id:int,debut_dm_datetime:datetime.datetime,fin_dm_datetime:datetime.datetime):
+    """use for read the emotion for a day and user"""
+    nb_row = conn.execute(messages.select(func.count(messages.c.dm_prob_happy)).where((messages.c.dm_datetime < debut_dm_datetime )&( messages.c.dm_datetime > fin_dm_datetime )&( messages.c.user_id == user_id)))
+    print('ici')
+    dm_prob_anger = conn.execute(messages.select(func.sum(messages.c.dm_prob_anger)).where(messages.c.dm_datetime < debut_dm_datetime & messages.c.dm_datetime > fin_dm_datetime & messages.c.user_id == user_id)) /nb_row
+    dm_prob_fear = conn.execute(messages.select(func.sum(messages.c.dm_prob_fear)).where(and_(messages.c.dm_datetime < debut_dm_datetime,messages.c.dm_datetime > fin_dm_datetime , messages.c.user_id == user_id))) /nb_row
+    dm_prob_happy = conn.execute(messages.select(func.sum(messages.c.dm_prob_happy)).where(and_(messages.c.dm_datetime < debut_dm_datetime,messages.c.dm_datetime > fin_dm_datetime , messages.c.user_id == user_id))) /nb_row
+    dm_prob_love = conn.execute(messages.select(func.sum(messages.c.dm_prob_love)).where(and_(messages.c.dm_datetime < debut_dm_datetime,messages.c.dm_datetime > fin_dm_datetime , messages.c.user_id == user_id))) /nb_row
+    dm_prob_sadness = conn.execute(messages.select(func.sum(messages.c.dm_prob_sadness)).where(and_(messages.c.dm_datetime < debut_dm_datetime,messages.c.dm_datetime > fin_dm_datetime , messages.c.user_id == user_id))) /nb_row
+    dm_prob_surprise = conn.execute(messages.select(func.sum(messages.c.dm_prob_surprise)).where(and_(messages.c.dm_datetime < debut_dm_datetime,messages.c.dm_datetime > fin_dm_datetime , messages.c.user_id == user_id))) /nb_row
+    return [dm_prob_anger,dm_prob_fear,dm_prob_happy,dm_prob_love,dm_prob_sadness,dm_prob_surprise]
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,3 +5,16 @@ class User(BaseModel):
     user_name:str
     user_last_meet:datetime
     user_next_meet:datetime
+
+class Message(BaseModel):
+    user_id:int
+    dm_text:str
+    dm_emotion:str
+    dm_prob_anger:float
+    dm_prob_fear:float 
+    dm_prob_happy:float  
+    dm_prob_love:float 
+    dm_prob_sadness:float 
+    dm_prob_surprise:float 
+    dm_prob_anger:float 
+    dm_datetime:datetime
